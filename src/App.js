985410cdc7c58e2dd715aa04fe7968c9f8a59ff2
Tim Romanowski <timromanowski15@gmail.com>
2022-07-18 16:10:40 +0200

    Add styledNavbar, products and login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import SharedLayout from './pages/SharedLayout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Products from './pages/Products';
 import Error from './pages/Error';
+import SingleProduct from './pages/SingleProduct';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path='about' element={<About />} />
-          <Route path='/products' element={<Products />} />
-          <Route path='/*' element={<Error />} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:productId' element={<SingleProduct />} />
+          <Route path='login' element={<Login setUser={setUser} />} />
+          <Route path='dashboard' element={<Dashboard user={user} />} />
+          <Route path='*' element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,14 @@
+import { Link, useParams } from 'react-router-dom';
+import products from '../data';
+
 const SingleProduct = () => {
+  const { productId } = useParams();
+  const product = products.find((product) => product.id === productId);
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <img src={product.image} alt='' />
+      <h5>{product.name}</h5>
+      <Link to='/products'>back to products</Link>
     </section>
   );
 };
